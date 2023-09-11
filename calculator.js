bf9ab615a8016411e139c8f063dd38022911558e
mocthball <56953651+mocthball@users.noreply.github.com>
2023-09-11 14:38:14 +1000

    Functions being assembled

--- calculator.js
+++ calculator.js
@@ -1,6 +1,36 @@
-let FIRST_NUM;
-let SECOND_NUM;
-let operator;
+let TOTALING;
+let SECOND_NUM = "";
+let operator = "";
+let first_operation = true;
+
+function key_entry(num){
+    if (SECOND_NUM.length > 12) {
+
+    }
+    else {
+        SECOND_NUM += num;
+    }
+    document.getElementById("current_operation").innerHTML = SECOND_NUM;
+}
+
+function operator_entry(operation) {
+    operator = operation;
+    let enums = document.getElementById("last_operation");
+    if (!enums.value && SECOND_NUM){
+        enums.innerHTML = SECOND_NUM + " " + operator;
+        TOTALING = Number(SECOND_NUM);
+        SECOND_NUM = "";
+        document.getElementById("current_operation").innerHTML = "";
+    }
+    else {
+        if (TOTALING){
+            console.log("test");
+        }
+        enums.innerHTML = TOTALING + " " + operator;
+    }
+
+}
+   
 
 function add(a,b) {
     return a + b;
@@ -29,4 +59,10 @@ function operate(operation, a, b) {
         case "divide":
             return divide(a,b);
     }
+}
+
+function clear_calc(){
+    first_operation = true;
+    FIRST_NUM = 0;
+    SECOND_NUM = 0;
 }
